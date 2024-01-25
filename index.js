6563e6910b281ca0514c721a6a2d7a0b35c7d34f
Michael Mgebrishvili <mmd920802@gmail.com>
2024-01-25 18:12:22 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+import { languages } from './cats.js';
+
+const page = document.querySelector('.page');
+const select = document.createElement('select');
+select.classList.add('form-select');
+page.appendChild(select);
+
+for (let language of languages) {
+    const option = document.createElement('option');
+    option.value = language;
+    option.textContent = language;
+
+    select.appendChild(option);
+}
+
+const text = document.querySelector('.text');
+
+select.addEventListener('change', (event) => {
+    const choiceSelect = event.target.value;
+    fetch('http://localhost:3000/')
+        .then(response => response.json())
+        .then((data) => {
+            text.textContent = `Переводится как "${data[choiceSelect]}"`;
+        })
+})
